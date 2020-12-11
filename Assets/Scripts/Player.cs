374e0cefe9a6fc5c37a04601f887d0b6b702e5cd
Alex <alexachtundzwanzig@gmail.com>
2020-12-11 03:15:43 +0100

    Player, Stone, (Cloud)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public BoxCollider2D groundTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
+        rb.mass = 1;
+        rb.drag = 0;
+        rb.angularDrag = 0;
+        rb.gravityScale = 4;
+        rb.freezeRotation = true;
+
+        gameObject.AddComponent<PlayerActions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<Rigidbody2D>();
+        GetComponent<Rigidbody2D>().mass = 1000;
+        GetComponent<Rigidbody2D>().drag = 0;
+        GetComponent<Rigidbody2D>().angularDrag = 0;
+        GetComponent<Rigidbody2D>().gravityScale = 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
