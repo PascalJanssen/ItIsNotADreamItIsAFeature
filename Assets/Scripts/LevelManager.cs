7c6078d9d03c4e90587d62b5567de43536670361
PascalJanssen <47032756+PascalJanssen@users.noreply.github.com>
2020-12-11 18:03:02 +0100

    alles ohne animation

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+}
