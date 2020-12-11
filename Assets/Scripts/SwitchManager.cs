b02b1a870d81416394f5be502b2df7645321cca6
PascalJanssen <47032756+PascalJanssen@users.noreply.github.com>
2020-12-11 05:20:36 +0100

    added wood

--- Assets/Scripts/SwitchManager.cs
+++ Assets/Scripts/SwitchManager.cs
@@ -24,6 +24,12 @@ public class SwitchManager
             Cloud.AddFunction(player);
             player.renderer.sprite = clickedObject.cloudSprite;
         }
+        else if (clickedObject.type.Equals("wood"))
+        {
+            Wood.RemoveFunction(clickedObject);
+            Wood.AddFunction(player);
+            player.renderer.sprite = clickedObject.woodSprite;
+        }
         else
         {
             return;
